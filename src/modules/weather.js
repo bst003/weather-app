f496e326a272ee8e1ac0bf112b29340d3cd2ef5d
bst003 <bstanner33@yahoo.com>
2022-08-05 20:55:33 -0400

    Set up fetch requests for metric and imperial units

--- src/modules/weather.js
+++ src/modules/weather.js
@@ -8,8 +8,25 @@ console.log(pubsub);
 // https://api.openweathermap.org/data/2.5/weather?q=hershey,pa,us&units=imperial&APPID=0d4e02963a957bd2504469564359c1de
 
 const weatherFunctions = (() => {
-    const getData = (location) => {
+    const getData = async (location) => {
         console.log(location);
+
+        const response = await Promise.all([
+            fetch(
+                `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=0d4e02963a957bd2504469564359c1de`,
+                { mode: "cors" }
+            ),
+            fetch(
+                `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&APPID=0d4e02963a957bd2504469564359c1de`,
+                { mode: "cors" }
+            ),
+        ]);
+
+        const weatherMetricData = await response[0].json();
+        const weatherImperialData = await response[1].json();
+
+        console.log(weatherMetricData);
+        console.log(weatherImperialData);
     };
 
     return {
