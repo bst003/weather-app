013ff4d701caa3491cb4ec3993048fd23d469723
bst003 <bstanner33@yahoo.com>
2022-08-05 21:15:06 -0400

    Create method to format data

--- src/modules/weather.js
+++ src/modules/weather.js
@@ -8,25 +8,58 @@ console.log(pubsub);
 // https://api.openweathermap.org/data/2.5/weather?q=hershey,pa,us&units=imperial&APPID=0d4e02963a957bd2504469564359c1de
 
 const weatherFunctions = (() => {
+    const _formatData = (metricData, imperialData) => {
+        console.log("test");
+
+        const formattedData = {
+            name: metricData.name,
+            main: metricData.weather[0].main,
+            description: metricData.weather[0].description,
+            metricData: {
+                temp: metricData.main.temp,
+                tempMax: metricData.main.temp_max,
+                tempMin: metricData.main.temp_min,
+            },
+            imperialData: {
+                temp: imperialData.main.temp,
+                tempMax: imperialData.main.temp_max,
+                tempMin: imperialData.main.temp_min,
+            },
+        };
+
+        return formattedData;
+    };
+
     const getData = async (location) => {
         console.log(location);
 
-        const response = await Promise.all([
-            fetch(
-                `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=0d4e02963a957bd2504469564359c1de`,
-                { mode: "cors" }
-            ),
-            fetch(
-                `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&APPID=0d4e02963a957bd2504469564359c1de`,
-                { mode: "cors" }
-            ),
-        ]);
+        try {
+            const response = await Promise.all([
+                fetch(
+                    `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=0d4e02963a957bd2504469564359c1de`,
+                    { mode: "cors" }
+                ),
+                fetch(
+                    `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&APPID=0d4e02963a957bd2504469564359c1de`,
+                    { mode: "cors" }
+                ),
+            ]);
 
-        const weatherMetricData = await response[0].json();
-        const weatherImperialData = await response[1].json();
+            const weatherMetricData = await response[0].json();
+            const weatherImperialData = await response[1].json();
 
-        console.log(weatherMetricData);
-        console.log(weatherImperialData);
+            console.log(weatherMetricData);
+
+            const formattedData = _formatData(
+                weatherMetricData,
+                weatherImperialData
+            );
+
+            console.log(formattedData);
+        } catch (err) {
+            console.log("this is an error");
+            console.log(err);
+        }
     };
 
     return {
