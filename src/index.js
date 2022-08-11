20358efa04ae1614fac2f95343060a8806a67812
bst003 <bstanner33@yahoo.com>
2022-08-10 22:29:01 -0400

    Remove un-needed comments

--- src/index.js
+++ src/index.js
@@ -5,29 +5,3 @@ import { domFunctions } from "./modules/domStuff";
 import { weatherFunctions } from "./modules/weather";
 
 pubsub.publish("pageLoad");
-
-/*
-
-Plan
-
-1. Create simple form to gather location (have placeholder text for city name, state code.
-    and country code).
-
-2. Put the data from the form into a single object.
-
-3. Use pubsub to publish that data to the weatherFunctions getData function.
-
-4. Use a Promise.All to fetch the data in both an imperial and metric format.
-
-5. Create a new object in getData to fomrat the location data with the location name,
-    temp (in metric/imperial), and man/description
-
-6. Push the new object to domStuff function (name tbd) to display info on page
-
-7. Account for errors with an error message below the search form.
-
-8. Create button to toggle between imperial and metric units.
-
-9. Make it look pretty
-
-*/
